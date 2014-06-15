90dcd825dfcc559dd0971739cf1d4675309bcf2e
risou <risou.f@gmail.com>
2014-06-15 23:46:13 +0900

    Initial Commit

---
+++ AccelSwitcher/RSFAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RSFAppDelegate.h
+//  AccelSwitcher
+//
+//  Created by risou on 2014/06/15.
+//  Copyright (c) 2014年 risou. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface RSFAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ AccelSwitcher/RSFAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  RSFAppDelegate.m
+//  AccelSwitcher
+//
+//  Created by risou on 2014/06/15.
+//  Copyright (c) 2014年 risou. All rights reserved.
+//
+
+#import "RSFAppDelegate.h"
+
+@implementation RSFAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
